722fdcdf9c685bce29308cc0ef01bbcac8b71afe
Ramesh N (Digital) <RA353444@wipro.com>
2018-01-06 10:59:25 +0530

    react-webapp-boilerplate

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+var path = require('path');
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+    app: [path.join(__dirname, "webclient", "clientapp.jsx")]
+  },
+  output: {
+    path: path.join(__dirname, "webclient", "dist"),
+    publicPath: "/dist/",
+    filename: "bundle.js"
+  },
+  module: {
+        loaders: [
+            {
+                test: /\.jsx$/,
+                loaders: ['babel?presets[]=react,presets[]=es2015,presets[]=stage-0']
+            }, {
+                test: /\.css$/,
+                loader: "style-loader!css-loader"
+            },
+            {
+                test: /\.(jpe?g|png|gif|svg)$/i,
+                loaders: ['file-loader']
+            }
+        ]
+  },
+  node: {
+  net: "empty",
+  fs: "empty"
+  },
+  watch:true,
+  resolve: {
+    extensions: ['', '.js', '.jsx', '/index.js', '/index.jsx']
+  },
+  plugins: [new HtmlWebpackPlugin({ template: path.resolve('./webclient/index.html') })]
+}
